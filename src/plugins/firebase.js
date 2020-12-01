2ec8576dae62ae1748afc8f43dcea8edd5de85f5
Onigiri <steinsgate.yn@gmail.com>
2020-12-01 16:37:08 +0800

    refactor: 提取 firebase 配置到根路径

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,17 +2,9 @@ import firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
+import {firebaseConfig} from "../../firebase.config";
 
-const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
-};
+
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
