bb9624fd8b3cc361f985d1e1049404dab30d96d5
Onigiri <steinsgate.yn@gmail.com>
2020-12-21 23:00:06 +0800

    style: formatting

--- firebase.config.js
+++ firebase.config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 };
