b2eceb4512c3fcd32d7347ac8491e469d33f40fd
Onigiri <steinsgate.yn@gmail.com>
2020-12-31 23:29:42 +0800

    refactor: 去除无用代码

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,3 @@
 import v1 from '@/api/v1';
 
 export default v1;
-
-export function APIResponseError(message: string) {
-  return Promise.reject({
-    message: message
-  });
-}
-
-export function APIResponseNotAsExpected(url: string) {
-  return Promise.reject({
-    message: `The response returned by the API[${url}] is not as expected.`
-  });
-}
